1e04a86be20acf048cba59f2628c850260afe78f
Muhammad Hamza Asif <m.hamza.asif.mha@gmail.com>
2020-11-02 09:43:50 -0800

    work on FAQ

---
+++ src/components/FAQ/index.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Button, Collapse } from 'antd';
+
+const { Panel } = Collapse;
+function FAQ() {
+    return (
+        <div className="block faqBlock" >
+            <div className="container-fluid">
+                <div className="titleHolder">
+                    <h2>Frequently Asked Questions</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores, veniam.</p>
+                </div>
+            <Collapse defaultActiveKey={['1']}>
+                <Panel header="How to setup the  theme?" key="1" >
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit inventore aut dicta quos ipsum porro ea, aperiam dolorum incidunt maiores quisquam at minima neque aliquam quo facere deserunt dolores! Dolor ipsa amet expedita mollitia facilis quos fugit harum accusantium minus voluptatem praesentium nostrum optio sequi, temporibus ipsum tempore blanditiis qui omnis reprehenderit pariatur commodi. Repudiandae, quo aspernatur nostrum blanditiis voluptatum laboriosam corporis illum, vero impedit voluptatem totam? Atque mollitia quaerat fugit officia, sunt nostrum alias asperiores pariatur rerum, odit optio soluta itaque temporibus ea adipisci corporis qui doloremque ab ut placeat ad ex? Eum nisi rerum, maiores quis sunt distinctio.</p>
+                </Panel>
+                <Panel header="How to change plan ?" key="2" >
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit inventore aut dicta quos ipsum porro ea, aperiam dolorum incidunt maiores quisquam at minima neque aliquam quo facere deserunt dolores! Dolor ipsa amet expedita mollitia facilis quos fugit harum accusantium minus voluptatem praesentium nostrum optio sequi, temporibus ipsum tempore blanditiis qui omnis reprehenderit pariatur commodi. Repudiandae, quo aspernatur nostrum blanditiis voluptatum laboriosam corporis illum, vero impedit voluptatem totam? Atque mollitia quaerat fugit officia, sunt nostrum alias asperiores pariatur rerum, odit optio soluta itaque temporibus ea adipisci corporis qui doloremque ab ut placeat ad ex? Eum nisi rerum, maiores quis sunt distinctio.</p>
+                </Panel>
+                <Panel header="How to Update Email?" key="3" >
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit inventore aut dicta quos ipsum porro ea, aperiam dolorum incidunt maiores quisquam at minima neque aliquam quo facere deserunt dolores! Dolor ipsa amet expedita mollitia facilis quos fugit harum accusantium minus voluptatem praesentium nostrum optio sequi, temporibus ipsum tempore blanditiis qui omnis reprehenderit pariatur commodi. Repudiandae, quo aspernatur nostrum blanditiis voluptatum laboriosam corporis illum, vero impedit voluptatem totam? Atque mollitia quaerat fugit officia, sunt nostrum alias asperiores pariatur rerum, odit optio soluta itaque temporibus ea adipisci corporis qui doloremque ab ut placeat ad ex? Eum nisi rerum, maiores quis sunt distinctio.</p>
+                </Panel>
+                <Panel header="How to Change Password?" key="4" >
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit inventore aut dicta quos ipsum porro ea, aperiam dolorum incidunt maiores quisquam at minima neque aliquam quo facere deserunt dolores! Dolor ipsa amet expedita mollitia facilis quos fugit harum accusantium minus voluptatem praesentium nostrum optio sequi, temporibus ipsum tempore blanditiis qui omnis reprehenderit pariatur commodi. Repudiandae, quo aspernatur nostrum blanditiis voluptatum laboriosam corporis illum, vero impedit voluptatem totam? Atque mollitia quaerat fugit officia, sunt nostrum alias asperiores pariatur rerum, odit optio soluta itaque temporibus ea adipisci corporis qui doloremque ab ut placeat ad ex? Eum nisi rerum, maiores quis sunt distinctio.</p>
+                </Panel>
+            </Collapse>
+            <div className="quickSupport">
+                <h3>Want Quick Support</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, mollitia!</p>
+                <Button type="primary"  size="large"> <i className="fas fa-envelope" ></i> Email your Question</Button>
+            </div>
+            </div>
+        </div>
+    )
+}
+
+export default FAQ
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,24 +1,26 @@
- import React,{lazy, Suspense} from 'react'
+import React, { lazy, Suspense } from 'react'
 
- //  IMPORT COMPONENTS
- import Hero from "../Hero";
+//  IMPORT COMPONENTS
+import FAQ from '../FAQ';
+import Hero from "../Hero";
+import Pricing from '../Pricing';
 
- // CODE-SPLITTING
-const About = lazy(()=> import('../About'))
-const Feature = lazy(()=> import('../Feature'))
-const Works = lazy(()=> import('../Works'))
+// CODE-SPLITTING
+const About = lazy(() => import('../About'))
+const Feature = lazy(() => import('../Feature'))
+const Works = lazy(() => import('../Works'))
 
 export default function Home() {
-     return (
-         <div className="main">
-             <Hero />
-             <Suspense fallback={<div>Loading ...</div>} >
+    return (
+        <div className="main">
+            <Hero />
+            <Suspense fallback={<div>Loading ...</div>} >
                 <About />
-
                 <Feature />
                 <Works />
-             </Suspense>
-         </div>
-     )
- }
- 
+            </Suspense>
+            <FAQ />
+            <Pricing/>
+        </div>
+    )
+}
