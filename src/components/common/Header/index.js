d62499435a93704f4b7351722cbd54a2bbe21c07
Muhammad Hamza Asif <m.hamza.asif.mha@gmail.com>
2020-11-04 09:24:07 -0800

    mobile menu

--- src/components/common/Header/index.js
+++ src/components/common/Header/index.js
@@ -1,8 +1,15 @@
-import React from 'react'
-import { Anchor } from 'antd';
+import React, {useState} from 'react'
+import { Anchor, Drawer, Button  } from 'antd';
 const { Link } = Anchor;
 
 export default function Header() {
+  const [visible, setVisible] = useState(false);
+  const showDrawer = () => {
+    setVisible(true);
+  };
+  const onClose = () => {
+    setVisible(false);
+  };
     return (
         <div className="container-fluid">
           <div className="header">
@@ -10,15 +17,38 @@ export default function Header() {
               <i className="fas fa-bolt"></i>
               <a href="www:/dask;l.com">Tech</a>
             </div>
+           <div className="mobileHidden">
             <Anchor targetOffset="65">
-              <Link href="#hero" title="Home" />
-              <Link href="#about" title="About" />
-              <Link href="#feature" title="Feature" />
-              <Link href="#works" title="Ho It Works" />
-              <Link href="#faq" title="FAQ" />
-              <Link href="#pricing" title="Pricing" />
-              <Link href="#contact" title="Contact" />
-            </Anchor>
+                <Link href="#hero" title="Home" />
+                <Link href="#about" title="About" />
+                <Link href="#feature" title="Feature" />
+                <Link href="#works" title="Ho It Works" />
+                <Link href="#faq" title="FAQ" />
+                <Link href="#pricing" title="Pricing" />
+                <Link href="#contact" title="Contact" />
+              </Anchor>
+           </div>
+           <div className="mobileVisible">
+           <Button type="primary" onClick={showDrawer}>
+             <i className="fas fa-bars"></i>
+           </Button>
+              <Drawer
+                placement="right"
+                closable={false}
+                onClose={onClose}
+                visible={visible}
+              >
+              <Anchor  onClose={onClose}>
+                        <Link href="#hero" title="Home"  />
+                        <Link href="#about" title="About" />
+                        <Link href="#feature" title="Feature" />
+                        <Link href="#works" title="Ho It Works" />
+                        <Link href="#faq" title="FAQ" />
+                        <Link href="#pricing" title="Pricing" />
+                        <Link href="#contact" title="Contact" />
+                      </Anchor>
+              </Drawer>
+            </div>
           </div>
         </div>
     )
